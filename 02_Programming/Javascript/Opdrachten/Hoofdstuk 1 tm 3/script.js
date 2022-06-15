a34aea6c6774f32d06cb21f00c8b4091906c3b86
Madhwi <madhwibalkaran@gmail.com>
2022-06-15 18:18:49 +0200

    hoofdstuk 1 tm 3 af

---
+++ 02_Programming/Javascript/Opdrachten/Hoofdstuk 1 tm 3/script.js
@@ -0,0 +1,110 @@
+
+//opdracht 3.1
+// let x = 22;
+// if ( x >= 5 && x <= 10 ) {
+//     console.log(x)
+// } else if ( x >= 11 && x <= 20) {
+//     console.log("y")
+// } else if ( x == 21 || x == 23 ) {
+//     console.log("g")
+// } else if ( x <= 35 || x >= 40 && x < 45) { 
+// console.log("k")
+// }
+
+//opdracht 3.2
+
+// let x = 20;
+// if ( x == 3 ) {
+//     console.log("dit is gelijk aan 3")
+// } else if ( x > 4 && x < 10) {
+//     console.log("Dit is groter dan 4")
+// } else if ( x > 11 ) {
+//     console.log("dit is groter dan 11")
+//  } else if ( x < 3 ) {
+//     console.log ("dit is kleiner dan 3")
+//  }
+
+
+//Vraag 3
+
+// let maand = 1;
+// if ( maand == 1) { 
+//     console.log("Januari")
+// } else if ( maand == 2 && maand < 3) {
+//     console.log("Februari")
+// } else if ( maand == 3 && maand < 4) {
+//     console.log("Maart")
+// } else if ( maand > 3 && maand < 5) {
+//     console.log("April")
+// } else if ( maand > 4 && maand < 6) {
+//     console.log("Mei")
+// } else if ( maand > 5 && maand < 7) {
+//     console.log("Juni")
+// } else if ( maand > 6 && maand < 8) {
+//     console.log("Juli")
+// } else if ( maand > 7 && maand < 9) {
+//     console.log("Augustus")
+// } else if ( maand > 8 && maand < 10) {
+//     console.log("September")
+// } else if ( maand > 9 && maand < 11) {
+//     console.log("Oktober")
+// } else if ( maand > 10 && maand < 12) {
+//     console.log("November")
+// } else if ( maand > 11 && maand < 13) {
+//     console.log("December")
+// } else if ( maand > 12 ) {
+//     console.log("Geen geldige maand!")
+// }
+
+//vraag 4
+
+// let maand = "15";
+// switch (maand) {
+//     case '1':
+//         console.log("Januari");
+//         break;
+//         case '2':
+//         console.log("Februari");
+//         break;
+//         case '3':
+//         console.log("Maart");
+//         break;
+//         case '4':
+//         console.log("April");
+//         break;
+//         case '5':
+//         console.log("Mei");
+//         break;
+//         case '6':
+//         console.log("Juni");
+//         break;
+//         case '7':
+//         console.log("Juli");
+//         break;
+//         case '8':
+//         console.log("Augustus");
+//         break;
+//         case '9':
+//         console.log("September");
+//         break;
+//         case '10':
+//         console.log("Oktober");
+//         break;
+//         case '11':
+//         console.log("November");
+//         break;
+//         case '12':
+//         console.log("December");
+//         break;
+//         default:
+//             console.log("Geen geldige maand!")
+// } 
+
+//vraag 5
+
+// let x = 3;
+// let results
+// results = x<=4 ? '2' : '5'
+// console.log(results)
+
+
--- 02_Programming/Javascript/Opdrachten/script.js
+++
@@ -1,7 +0,0 @@
-let x = 30;
-let y = 44; 
-if (30 || 44)
-console.log(x);
-
-
-
