63eee8ee3e342b8e6c77c2469c2d38678df41a7e
Madhwi <madhwibalkaran@gmail.com>
2022-06-17 20:32:24 +0200

    Javascript

--- 02_Programming/Javascript/Opdrachten/Hoofdstuk 4/main.js
+++ 02_Programming/Javascript/Opdrachten/Hoofdstuk 4/main.js
@@ -18,10 +18,10 @@ function getText(test) {
       
 }
 
-var z = "Result"
-function getNumber(number) {
+// var z = "Result"
+// function getNumber(number) {
     
-}
+// }
 
 //vraag 4
 
@@ -36,3 +36,55 @@ document.getElementById("demo").innerHTML = collection.length;
 //queryselector
 
 document.querySelector("h3, h4").style.backgroundColor = "red";
+
+//queryselectorAll
+// function myfunction() { 
+// let container = document.querySelector("#container");
+// let matches = document.querySelectorAll("div.div1 > p");
+// let change; 
+// for ()
+// }
+
+// const container = document.getElementById("container")
+// container.style.backgroundColor = "red"
+
+const container = document.querySelector("#container")
+changeColor(container)
+function changeColor(element) {
+    element.style.backgroundColor = "aquamarine"
+}
+
+// const div1 = document.querySelectorAll("#container.div1 > p")
+// kleurP.forEach(kleurP)
+// function kleurP(test) {
+//     test.style.backgroundColor = "red"
+// }
+
+function myFunction() {
+    document.getElementById("oefening1").innerHTML = "";
+
+}
+
+
+// var y = document.getElementById("firstInputElement");
+// function getText(test) {
+//   var x = console.log(test.value);
+//  return (x *2);
+      
+// }
+
+
+// function randomNumber() {
+//     let x = 1;
+// console.log(Math.round(0 >= x < 1))
+// }
+
+// function randomNumber() {
+//     let x = 15;
+// console.log(Math.floor(Math.random() * x))
+// }
+
+// Vraag 4.4
+
+
+
