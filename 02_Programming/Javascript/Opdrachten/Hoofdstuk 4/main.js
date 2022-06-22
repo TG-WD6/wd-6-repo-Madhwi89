7d2e3eec5badde336cf2e5559c4a98c43c0ab2cd
Madhwi <madhwibalkaran@gmail.com>
2022-06-22 20:25:48 +0200

    javascript

--- 02_Programming/Javascript/Opdrachten/Hoofdstuk 4/main.js
+++ 02_Programming/Javascript/Opdrachten/Hoofdstuk 4/main.js
@@ -188,7 +188,7 @@ for(const i in nlsport_2){
   console.log(nlsport_2[i])
 }
 
-//for in getallen!!!!
+//for in getallen!!!! 5.2 vraag 3 done
 let getallen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 for (getallen = 1; getallen < 22; getallen++) {
   if (getallen % 2 === 0) {
@@ -197,3 +197,193 @@ for (getallen = 1; getallen < 22; getallen++) {
 }
 
 
+// initialize, condition, increment
+
+// let getallen_1 = 0
+// while (getallen_1 < 3) {
+// console.log("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20"); 
+//   getallen_1++;
+// }
+
+//opdracht 5.2 nog een keer doen!!
+// for (let i=1; i<=15; i++ ){
+//   console.log(i);
+//   }
+  //opdracht 5.2  vraag 4 en 5 nog een keer doen^!!
+
+  //fibonacci
+  var a = 0, b = 1;
+  console.log(a) 
+  console.log(b)
+for(var i=0; i<=47;i++){
+  var temp = a + b; 
+  console.log(temp);
+  a = b;
+  b = temp;
+}
+
+
+//bubblesort array
+function bubbleSort(array) {
+  for(let i=0; i<array.length -1; i++){
+    for(let j=0; j<array.length -1 -i; j++) {
+      if ( array[j] > array[j+1]) {
+        const temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+  return array;
+}
+console.log(bubbleSort([2, 7, 5, 10, 4, 9, 3, 1, 8, 6]))
+
+//Opdracht 5.3
+const dutchSports_1 = ["Voetbal;", "Hockey", "Schaatsen"]
+console.log(dutchSports_1) 
+console.log(dutchSports_1.push("Zeilen", "Zwemmen"))
+console.log(dutchSports_1)
+console.log(dutchSports_1.unshift ("Volleybal"))
+console.log(dutchSports_1)
+
+const ballSports = ["Voetbal", "Volleybal"]
+console.log(ballSports)
+console.log(dutchSports_1.shift () + " is a ballsport")
+console.log(dutchSports_1.splice (0, 1) + " is a ballsport ")
+console.log(dutchSports_1)
+console.log(dutchSports_1[0], dutchSports_1[1], dutchSports_1[2], dutchSports_1[3])
+
+const dutchSports_1Length = dutchSports_1.length;
+console.log(dutchSports_1Length)
+
+const sportsLength = ["Hockey", "Schaatsen", "Zeilen", "Zwemmen"]
+console.log(sportsLength[0].length)
+console.log(sportsLength[1].length)
+console.log(sportsLength[2].length)
+console.log(sportsLength[3].length)
+
+
+//Opdracht 5.4
+
+let arrayLike = document.querySelectorAll('div')
+console.log(arrayLike)
+const nodArray = Array.from(arrayLike)
+console.log(nodArray)
+
+//.filter odd
+const filterArray = [2, 8, 15, 19, 35, 48]
+function isOdd(x){
+  return x%2
+}
+const filterOutputOdd = filterArray.filter(isOdd)
+console.log(filterOutputOdd)
+
+//.filter even
+function isEven(x){
+  return x%2===0
+}
+const filterOutputEven = filterArray.filter(isEven)
+console.log(filterOutputEven)
+
+//.find
+const MyName = ["Madhwi", "Renukadevi", "Balkaran"]
+const res = MyName.find(findRenukadevi)
+function findRenukadevi(item){
+  return item === "Renukadevi";
+}
+console.log(res)
+
+//.find nummer 2
+const persons = [
+  { 
+    Name: "Madhwi",
+    Age: 33
+  }, 
+  {
+    Name: "Vikas",
+    Age: 34
+  }, 
+  {
+    Name: "Pietje",
+    Age: 5
+  }
+];
+
+const res_1 = MyName.find(findMadhwi);
+
+function findMadhwi(item){
+  return item === "Madhwi";
+}
+console.log(res_1);
+
+
+// .some 
+const zoekCijfer = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+const resultaat = zoekCijfer.some(greaterThanFour)
+
+function greaterThanFour(item){
+return item > 4
+}
+
+console.log(resultaat)
+
+
+
+const adultAge = [
+  {
+    Naam: "Madhwi",
+    Leeftijd: 33
+  },
+  {
+    Naam: "Klaasje",
+    Leeftijd: 12
+  },
+  {
+    Naam: "Jantje",
+    Leeftijd: 18
+  }
+]
+
+const meerderjarig = adultAge.some(isAdult)
+
+function isAdult(naam){
+  return naam.Leeftijd >=18;
+}
+
+console.log(meerderjarig)
+
+//.every
+const everyNumber = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+
+
+const resultaat_1 = everyNumber.every(isPositive)
+
+function isPositive(item){ 
+return item > 9
+}
+console.log(resultaat_1)
+
+//.every 2
+const persons_1 = [
+  {
+    name: "Madhwi"
+  },
+  {
+    name: "Pietje"
+  },
+  { surname: "Klaasje"
+}
+]
+
+const res_2 = persons_1.every(person => person.name != undefined)
+console.log(res_2)
+
+//.includes
+const fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+const fruits_1 = fruits.includes("Banana")
+if(fruits_1) {
+  console.log("Yes Banana!!")
+} else {
+  console.log("No Banana.. ")
+}
