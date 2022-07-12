05acfe8e4f5e481dc805878648ac3c705d142ae8
Madhwi <madhwibalkaran@gmail.com>
2022-07-12 18:15:45 +0200

    search

--- 02_Programming/Javascript/eindopdracht/eindopdracht javascript/Searchbar/main.js
+++ 02_Programming/Javascript/eindopdracht/eindopdracht javascript/Searchbar/main.js
@@ -1,2 +1,25 @@
 let search = {''}
 console.log(search)
+
+// localStorage.setItem("name": "Madhwi",)
+
+
+// Write a local item..
+window.localStorage.setItem("myKey", "myValue");
+
+// Read a local item..
+var theItemValue = window.localStorage.getItem("myKey");
+
+// Check for changes in the local item and log them..
+window.addEventListener('storage', function(event) {
+    console.log('The value for ' + event.key + ' was changed from' + event.oldValue + ' to ' + event.newValue);
+}, false);
+
+// Check for HTML5 Storage..
+function supports_html5_storage() {
+    try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
