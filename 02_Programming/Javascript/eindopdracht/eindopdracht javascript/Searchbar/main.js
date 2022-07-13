216c0efc44b7df9a2abaf455cc336b21aeecc1ed
Madhwi <madhwibalkaran@gmail.com>
2022-07-13 17:35:51 +0200

    search

--- 02_Programming/Javascript/eindopdracht/eindopdracht javascript/Searchbar/main.js
+++ 02_Programming/Javascript/eindopdracht/eindopdracht javascript/Searchbar/main.js
@@ -1,25 +1,41 @@
-let search = {''}
-console.log(search)
+// const input = [] 
 
-// localStorage.setItem("name": "Madhwi",)
+// search.addEventListener("event type", () => {
+
+//     const searchInput = document.getElementsByClassName('.search_results')
+// })
 
 
-// Write a local item..
-window.localStorage.setItem("myKey", "myValue");
+// // Write a local item..
+// window.localStorage.setItem("myKey", "myValue");
 
-// Read a local item..
-var theItemValue = window.localStorage.getItem("myKey");
+// // Read a local item..
+// var theItemValue = window.localStorage.getItem("myKey");
 
-// Check for changes in the local item and log them..
-window.addEventListener('storage', function(event) {
-    console.log('The value for ' + event.key + ' was changed from' + event.oldValue + ' to ' + event.newValue);
-}, false);
+// // Check for changes in the local item and log them..
+// window.addEventListener('storage', function(event) {
+//     console.log('The value for ' + event.key + ' was changed from' + event.oldValue + ' to ' + event.newValue);
+// }, false);
 
-// Check for HTML5 Storage..
-function supports_html5_storage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
-    }
-}
+// // Check for HTML5 Storage..
+// function supports_html5_storage() {
+//     try {
+//         return 'localStorage' in window && window['localStorage'] !== null;
+//     } catch (e) {
+//         return false;
+//     }
+// }
+
+const searchOptions = [
+    {'Techgrounds'},
+    {'Webdeveloper'},
+    {'HTML'},
+    {'CSS'},
+    {'Javascript'}
+]
+
+const searchInput = document.getElementById('search')
+
+searchInput.addEventListener('input', () => {
+    console.log('input event worked')
+})
